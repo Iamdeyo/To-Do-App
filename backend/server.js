02b7505812d96dd5c223040d3aa7206ff3387ca8
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2023-05-19 22:41:42 +0100

    error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import router from './routes/index.js';
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js';
 
 dotenv.config();
 
@@ -11,6 +12,9 @@ app.use('/api', router);
 app.get('/', (req, res) => {
   res.status(200).send('welcome to Todo Api');
 });
+
+// error handler middlewares
+app.use(notFound).use(errorHandler);
 
 const port = process.env.PORT || 8080;
 
