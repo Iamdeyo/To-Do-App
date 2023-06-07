47872b4598c4b646644f013ed1368ab95fdf3c41
Ademola Taiwo <102713835+Iamdeyo@users.noreply.github.com>
2023-06-07 07:00:54 +0100

    cors error fix

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app
   .use(
     cors({
       credentials: true,
-      origin: ['http://127.0.0.1:3000', 'https://dtodo.onrender.com/'],
+      origin: 'https://dtodo.onrender.com',
     })
   );
 app.use('/api', router);
